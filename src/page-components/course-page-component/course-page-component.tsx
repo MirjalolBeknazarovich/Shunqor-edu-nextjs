92bec26d41507c53dbb67e4e31ed8ebb5134338d
Mirjalol <turayev9496@gmail.com>
2023-04-15 00:56:10 +0500

    Sort Component

--- src/page-components/course-page-component/course-page-component.tsx
+++ src/page-components/course-page-component/course-page-component.tsx
@@ -1,17 +1,28 @@
 import { CoursePageComponentProps } from "./course-page-component.props";
 import styles from './course-page-component.module.css';
 import { Advantages, Heading, Hhdata, Product, Sort, Tag, Text } from "@/components";
+import { useReducer } from "react";
+import { sortReducer } from "./sort.reducer";
+import { SortEnum } from "@/components/sort/sort.props";
 
 const CoursePageComponent = ({ page, products }: CoursePageComponentProps): JSX.Element => {
+  
+  const [state, dispatch] = useReducer(sortReducer, { sort: SortEnum.Rating, products: products });
+
+  const setSort = (sort: SortEnum) => {
+    dispatch({type: sort})
+  }
+  
+
   return (
     <div className={styles.wrapper}>
       {/* Title */}
         <div className={styles.title}>
             <Heading tag='h1'>{page.title}</Heading>
-            <Sort />
+            <Sort sort={state.sort} setSort={setSort} />
         </div>
         {/* Products */}
-        <div>{products && products.map((c, index) => <Product key={index} product={c} />)}</div>
+        <div>{state.products && state.products.map((c, index) => <Product key={index} product={c} />)}</div>
         {/* Vactions */}
         <div className={styles.hhTitle}>
           <Heading tag='h2'>Vactions - {page.category}</Heading>

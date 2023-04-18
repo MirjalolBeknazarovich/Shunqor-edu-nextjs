8bb2d05e73a8779a2036d2998e6e6a34e479040f
Mirjalol <turayev9496@gmail.com>
2023-04-18 22:36:08 +0500

    NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import "nprogress/nprogress.css";
+import * as React from "react";
+import NProgress from "nprogress";
+import Router from 'next/router';
+
+NProgress.configure({showSpinner: false})
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+ 
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+ 
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
   return <Component {...pageProps} />
 }
